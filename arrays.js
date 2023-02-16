466f5ac58bacef1ae8d823f07180f353a80a54ad
mayurtnn <mayurt@zignuts.com>
2023-02-16 14:29:09 +0530

    after noon commit

---
+++ arrays.js
@@ -0,0 +1,57 @@
+let marks= [10,20,30, null,"Man"]
+// console.log(marks.length)
+// for(let i; i<=marks.length; i++){
+   
+// 
+
+// console.log(marks)
+
+// for( let k in marks){
+//     console.log(marks[k])
+// }
+// marks[2]=1010;
+// console.log(marks)
+
+// let r= marks.shift(10);
+// console.log(r, marks)
+
+
+// console.log(marks)
+// let r1=marks.unshift("Man")
+// console.log(r1)
+
+
+
+// let comapare =(a,b) =>{
+//     return a-b
+
+// }
+
+// let num=[10,321.90,356,3,32.55,1]
+// console.log(num.sort(comapare))
+
+
+const num=[10,11,40,34,56]
+// console.log(num.splice(1,1,20,30))
+// console.log(num)
+
+// console.log(num.slice(1,4))
+
+// for( let i=0;i<num.length;i++){
+//     console.log("The number at "+i+ " is  " +num[i] )
+// }
+// num.forEach((item) => {
+//     console.log(item+5-2+5)
+// })
+
+
+// let n="M-a-y-y-u-r"
+// console.log(n)
+// let arr_n=Array.from(n)
+// console.log(arr_n)
+
+
+// for( let i in num){
+//     console.log(num[i])
+// }
+
---
+++ strings.js
@@ -0,0 +1,54 @@
+let boy1="Raj";
+let boy2="Man";
+// let sentence=`boy2  is a friemd of boy1`;
+
+// Interpolation or template literals
+// let sentence=`${boy2}  is a friemd of ${boy1}`;
+// console.log(sentence)
+
+
+// let fri='Bana\nna';
+// console.log(fri)
+
+
+
+let text = "I am a boy and you atre your ok";
+// let part = text.slice(-12,-6);
+
+// let part = text.substring(5,8);
+// let part = text.substr(2,7);
+// console.log(part)
+
+
+// let name=" Harry Tanna";
+// let m3=name.replace(/Harry/g, "Mayur")
+// console.log(m3)
+// console.log(m3.concat("Rajubhai"))
+
+// practise-questions
+
+// console.log("har\"".length)
+
+
+// let name="mayur"
+// console.log(name.toUpperCase())
+
+
+let name="pleame give Rs 1000";
+// console.log(name.slice(-4))
+// console.log(name.replace(name.charAt(4),"s"))
+
+
+let marks= [10,20,30, null,"Man"]
+// console.log(marks.length)
+// for(let i; i<=marks.length; i++){
+   
+// 
+
+// console.log(marks)
+
+// for( let k in marks){
+//     console.log(marks[k])
+// }
+// marks[2]=1010;
+// console.log(marks)
