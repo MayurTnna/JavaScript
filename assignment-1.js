a5e8946c036c5685575d4165646319e43d20da9f
mayurtnn <mayurt@zignuts.com>
2023-02-21 11:34:40 +0530

    assignment commits

---
+++ assignment-1.js
@@ -0,0 +1,16 @@
+let s="1.5, 2.3, 3.1, 4, 5.5, 6, 7, 8, 9, 10.9"
+
+function sumofdigits(s)
+{
+    var ar = s.split(",");
+    
+    var sum = 0;
+    
+    for(var n of ar)
+    {
+        sum += Number.parseFloat(n);
+    }
+    
+    return sum;
+}
+console.log("Answer of given string:-"+s+ " is " +sumofdigits(s))
---
+++ assignment-2.js
@@ -0,0 +1,42 @@
+ let str= "foo8bar8cat2tc2"
+// sum=0
+// nums=[]
+
+
+// sumstring=(str)=>{
+//     for( let k in str)
+//     {
+//         if(typeof(str.k)==="number")
+//         num.push([str[k]])
+//         console.log(num[k])
+//     }
+//     console.log(num[k])
+// }
+let m22=0
+let tot
+
+for( let k in str)
+{
+    console.log(Number(str[k]))
+  
+
+}
+
+sumOfDigitsFromString =(str)=>{
+    let nums = [];
+    let sum = 0;
+  
+    for (let i in str) {
+      if (!isNaN(Number(str[i]))) {
+        nums.push(Number(str[i]));
+      }
+    }
+    console.log(nums);
+    for (let i = 0; i < nums.length; i++) {
+      sum =sum+ nums[i];
+    }
+    return sum;
+  }
+  
+
+  console.log("The sum of numbers from given string "+str+" is "  +sumOfDigitsFromString(str));
